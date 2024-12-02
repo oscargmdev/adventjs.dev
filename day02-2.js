5528cbf49daf241fda6f6041c67e54d62f143ede
oscargmdev <oskargm@gmail.com>
2024-12-02 21:03:47 +0100

    Create day02-2.js

---
+++ day02-2.js
@@ -0,0 +1,13 @@
+function createFrame(names) {
+
+  let length = 0
+  names.forEach(value => length = Math.max(value.length, length))
+
+  let tapa = '*'.repeat(length + 4)
+
+  let result = names.reduce((previous, current) => {
+    return previous + '* ' + current.padEnd(length) + ' *\n'
+  }, '\n')
+
+  return tapa + result + tapa
+}
