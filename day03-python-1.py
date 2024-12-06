be30b20e182ad72cc3090263ebb081e136247a85
oscargmdev <oskargm@gmail.com>
2024-12-06 22:04:40 +0100

    Create day03-python-1.py

---
+++ day03-python-1.py
@@ -0,0 +1,16 @@
+# Puntuación: 4 estrellas
+
+def organizeInventory(inventory):
+  result = {}
+
+  for item in inventory:
+  
+    if !result[value.category] :
+      result[value.category] = {}
+ 
+    if !result[value.category][value.name] :
+      result[value.category][value.name] = 0
+
+    result[value.category][value.name] += value.quantity
+
+  return result
