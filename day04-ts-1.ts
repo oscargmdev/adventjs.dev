0503c38bc1e954d849cf6fa5cdb730aecdf3fb76
oscargmdev <oskargm@gmail.com>
2024-12-06 21:48:52 +0100

    Create day04-ts-1.ts

---
+++ day04-ts-1.ts
@@ -0,0 +1,15 @@
+// Puntuación: 4 estrellas
+
+function createXmasTree(height: number, ornament: string): string {
+  let result = ''
+
+  for(let i=0;i<height;i++) {
+    result += '_'.repeat(height-i-1) + ornament.repeat(i*2+1) + '_'.repeat(height-i-1) + '\n'
+  }
+
+  let base = '_'.repeat(height-1) + '#' + '_'.repeat(height-1)
+
+  result += base + '\n' + base
+
+  return result
+}
