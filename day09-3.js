3047602a32b8010c9ca8936e359bb7ee1edf87a9
oscargmdev <oskargm@gmail.com>
2024-12-10 00:49:14 +0100

    Create day09-3.js

---
+++ day09-3.js
@@ -0,0 +1,36 @@
+// Puntuación: 5 estrellas
+
+/**
+ * @param {string[]} board - Represent the train situation
+ * @param {'U' | 'D' | 'R' | 'L' } mov - Movement direction
+ * @returns {'none' | 'crash' | 'eat'}
+ */
+function moveTrain(board, mov) {
+
+  let head = null
+
+  let x, y
+  for(y=0;y<board.length;y++) {
+    x = board[y].indexOf('@')
+    if( x > -1) break
+  }
+
+  const movements = {
+    'U': { dx: 0, dy: -1},
+    'D': { dx: 0, dy: 1},
+    'L': { dx: -1, dy: 0},
+    'R': { dx: 1, dy: 0}
+  }
+
+  let {dx, dy} = movements[mov]
+  x += dx
+  y += dy
+
+  if( !board[y] || !board[y][x] ) return 'crash'
+
+  let value = board[y][x]
+  if( value === 'o') return 'crash'
+  if( value === '*') return 'eat'
+
+  return 'none'
+}
