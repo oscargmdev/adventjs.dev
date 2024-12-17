d9aeef3add02f14793afbe140ed226c90790412c
oscargmdev <oskargm@gmail.com>
2024-12-17 19:42:29 +0100

    Create day17-1.js

---
+++ day17-1.js
@@ -0,0 +1,29 @@
+// Puntuación: 4 estrellas
+
+/**
+ * @param {boolean[][]} grid
+ * @returns {number[][]}
+ */
+function detectBombs(grid) {
+
+  function at(i,j) {
+    return (grid[i]?.[j] ?? false) ? 1 : 0
+  }
+
+  function sum(i,j) {
+    return at(i-1,j-1) + at(i-1, j) + at(i-1, j+1) +
+      at(i,j-1) + at(i,j+1) +
+      at(i+1, j-1) + at(i+1, j) + at(i+1, j+1)
+  }
+
+  let resp = new Array()
+
+  for(let i=0;i<grid.length;i++) {
+    resp.push(new Array())
+    for(let j=0;j<grid[i].length;j++) {
+      resp[i].push( sum(i,j) )
+    }
+  }
+
+  return resp
+}
